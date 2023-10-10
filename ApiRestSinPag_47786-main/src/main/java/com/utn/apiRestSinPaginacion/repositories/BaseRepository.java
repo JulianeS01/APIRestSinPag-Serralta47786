3ccb64f610014a858a23fc9dce9a5a4a6cee9477
JulianeS01 <90938368+JulianeS01@users.noreply.github.com>
2023-10-10 12:47:49 -0300

    .

--- ApiRestSinPag_47786-main/src/main/java/com/utn/apiRestSinPaginacion/repositories/BaseRepository.java
+++ ApiRestSinPag_47786-main/src/main/java/com/utn/apiRestSinPaginacion/repositories/BaseRepository.java
@@ -3,7 +3,7 @@ package com.utn.apiRestSinPaginacion.repositories;
 import com.utn.apiRestSinPaginacion.entities.Base;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.yaml.snakeyaml.events.Event;
+
 
 import java.io.Serializable;
 
